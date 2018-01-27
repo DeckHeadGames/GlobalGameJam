c2fe3baf8191dc62859c18c79d32a910b37b2aad
unknown <jamescol@usc.edu>
2018-01-26 22:52:19 -0800

    Basic AI setup

---
+++ ggj2018/Intermediate/Build/Win64/UE4Editor/Inc/ggj2018/TP_ThirdPersonCharacter.generated.h
@@ -0,0 +1,94 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "ObjectMacros.h"
+#include "ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef GGJ2018_TP_ThirdPersonCharacter_generated_h
+#error "TP_ThirdPersonCharacter.generated.h already included, missing '#pragma once' in TP_ThirdPersonCharacter.h"
+#endif
+#define GGJ2018_TP_ThirdPersonCharacter_generated_h
+
+#define ggj2018_Source_ggj2018_TP_ThirdPerson_TP_ThirdPersonCharacter_h_12_RPC_WRAPPERS
+#define ggj2018_Source_ggj2018_TP_ThirdPerson_TP_ThirdPersonCharacter_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define ggj2018_Source_ggj2018_TP_ThirdPerson_TP_ThirdPersonCharacter_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesATP_ThirdPersonCharacter(); \
+	friend GGJ2018_API class UClass* Z_Construct_UClass_ATP_ThirdPersonCharacter(); \
+public: \
+	DECLARE_CLASS(ATP_ThirdPersonCharacter, ACharacter, COMPILED_IN_FLAGS(0), 0, TEXT("/Script/ggj2018"), NO_API) \
+	DECLARE_SERIALIZER(ATP_ThirdPersonCharacter) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define ggj2018_Source_ggj2018_TP_ThirdPerson_TP_ThirdPersonCharacter_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesATP_ThirdPersonCharacter(); \
+	friend GGJ2018_API class UClass* Z_Construct_UClass_ATP_ThirdPersonCharacter(); \
+public: \
+	DECLARE_CLASS(ATP_ThirdPersonCharacter, ACharacter, COMPILED_IN_FLAGS(0), 0, TEXT("/Script/ggj2018"), NO_API) \
+	DECLARE_SERIALIZER(ATP_ThirdPersonCharacter) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define ggj2018_Source_ggj2018_TP_ThirdPerson_TP_ThirdPersonCharacter_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ATP_ThirdPersonCharacter(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ATP_ThirdPersonCharacter) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ATP_ThirdPersonCharacter); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ATP_ThirdPersonCharacter); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ATP_ThirdPersonCharacter(ATP_ThirdPersonCharacter&&); \
+	NO_API ATP_ThirdPersonCharacter(const ATP_ThirdPersonCharacter&); \
+public:
+
+
+#define ggj2018_Source_ggj2018_TP_ThirdPerson_TP_ThirdPersonCharacter_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ATP_ThirdPersonCharacter(ATP_ThirdPersonCharacter&&); \
+	NO_API ATP_ThirdPersonCharacter(const ATP_ThirdPersonCharacter&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ATP_ThirdPersonCharacter); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ATP_ThirdPersonCharacter); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(ATP_ThirdPersonCharacter)
+
+
+#define ggj2018_Source_ggj2018_TP_ThirdPerson_TP_ThirdPersonCharacter_h_12_PRIVATE_PROPERTY_OFFSET \
+	FORCEINLINE static uint32 __PPO__CameraBoom() { return STRUCT_OFFSET(ATP_ThirdPersonCharacter, CameraBoom); } \
+	FORCEINLINE static uint32 __PPO__FollowCamera() { return STRUCT_OFFSET(ATP_ThirdPersonCharacter, FollowCamera); }
+
+
+#define ggj2018_Source_ggj2018_TP_ThirdPerson_TP_ThirdPersonCharacter_h_9_PROLOG
+#define ggj2018_Source_ggj2018_TP_ThirdPerson_TP_ThirdPersonCharacter_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	ggj2018_Source_ggj2018_TP_ThirdPerson_TP_ThirdPersonCharacter_h_12_PRIVATE_PROPERTY_OFFSET \
+	ggj2018_Source_ggj2018_TP_ThirdPerson_TP_ThirdPersonCharacter_h_12_RPC_WRAPPERS \
+	ggj2018_Source_ggj2018_TP_ThirdPerson_TP_ThirdPersonCharacter_h_12_INCLASS \
+	ggj2018_Source_ggj2018_TP_ThirdPerson_TP_ThirdPersonCharacter_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define ggj2018_Source_ggj2018_TP_ThirdPerson_TP_ThirdPersonCharacter_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	ggj2018_Source_ggj2018_TP_ThirdPerson_TP_ThirdPersonCharacter_h_12_PRIVATE_PROPERTY_OFFSET \
+	ggj2018_Source_ggj2018_TP_ThirdPerson_TP_ThirdPersonCharacter_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	ggj2018_Source_ggj2018_TP_ThirdPerson_TP_ThirdPersonCharacter_h_12_INCLASS_NO_PURE_DECLS \
+	ggj2018_Source_ggj2018_TP_ThirdPerson_TP_ThirdPersonCharacter_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID ggj2018_Source_ggj2018_TP_ThirdPerson_TP_ThirdPersonCharacter_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
--- ggj2018/Intermediate/Build/Win64/UE4Editor/Inc/ggj2018/ggj2018.init.gen.cpp
+++ ggj2018/Intermediate/Build/Win64/UE4Editor/Inc/ggj2018/ggj2018.init.gen.cpp
@@ -19,8 +19,8 @@ void EmptyLinkFunctionForGeneratedCodeggj2018_init() {}
 			static const UE4CodeGen_Private::FPackageParams PackageParams = {
 				"/Script/ggj2018",
 				PKG_CompiledIn | 0x00000000,
-				0x6A5EC021,
-				0x5F1570C0,
+				0x18FF159F,
+				0x3D292AE1,
 				nullptr, 0,
 				METADATA_PARAMS(nullptr, 0)
 			};
